44b87ab1fe392c0502e95fa9144bc7523776ee0d
smm3123 <syedmahdi3123@gmail.com>
2023-03-14 23:58:38 -0500

    Add open ai sentiment analysis example

---
+++ open_ai_sentiment_analysis.py
@@ -0,0 +1,39 @@
+from dotenv import load_dotenv
+import os
+import openai
+
+load_dotenv()
+openai.api_key = os.getenv("API_KEY")
+
+prompt = "Determine whether the sentiment in each of these responses is positive, negative, or neutral:\n\n"
+
+responses = [
+    "Could do better work next time, currently still progress that needs to be made",
+    "Did a good job in the classroom, students listened to them",
+    "Amazing work, keep it up!",
+    "Not good, not bad",
+    "While this was enjoyable enough, it wasn't really great...in fact, I'd go so far as to say it wasn't great at all",
+]
+
+for i in range(len(responses)):
+    prompt += f"{i + 1}. {responses[i]}\n"
+
+print(prompt)
+
+
+response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt=prompt,
+  temperature=0,
+  max_tokens=60,
+  top_p=1.0,
+  frequency_penalty=0.0,
+  presence_penalty=0.0
+)
+
+data = response.choices[0].text
+sentiments = data.split("\n")
+
+print(sentiments)
+print()
+print(response)
