139ff8d52d55f81d45095437cd089143f66068e8
smm3123 <syedmahdi3123@gmail.com>
2023-03-15 00:03:19 -0500

    Add link to list of models

--- open_ai_sentiment_analysis.py
+++ open_ai_sentiment_analysis.py
@@ -8,6 +8,7 @@ openai.api_key = os.getenv("API_KEY")
 # prompt = "Determine whether the sentiment in each of these responses is positive, negative, or neutral:\n\n"
 prompt = "Determine what the sentiment of each of these responses is on a scale of 1 - 5, where 1 is the most negative, 3 is neutral, and 5 is positive:\n"
 
+# List of models: https://platform.openai.com/docs/models/gpt-3
 responses = [
     "Could do better work next time, currently still progress that needs to be made",
     "Did a good job in the classroom, students listened to them",
